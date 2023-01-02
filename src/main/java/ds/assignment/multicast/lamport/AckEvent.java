3391db97bd7622cfc844fcef91612e6745a0e831
luismdsleite <luismdsleite@gmail.com>
2023-01-02 18:42:52 +0000

    Change AckEvent structure

--- src/main/java/ds/assignment/multicast/lamport/AckEvent.java
+++ src/main/java/ds/assignment/multicast/lamport/AckEvent.java
@@ -2,18 +2,27 @@ package ds.assignment.multicast.lamport;
 
 public class AckEvent extends LamportEvent {
 
-  public AckEvent(int pid, long logical_clock, long eventID) {
+  private int eventPID;
+
+  public int getEventPID() {
+    return eventPID;
+  }
+
+  public AckEvent(int pid, long logical_clock, long eventID, int eventPID) {
     super(pid, logical_clock, eventID);
+    this.eventPID = eventPID;
   }
 
   @Override
   public String toString() {
     return (
-      "Ack Event " +
+      "Ack Event ID:" +
       super.getEventID() +
-      " " +
+      " Event_PID:" +
+      super.getEventID() +
+      " PID:" +
       this.getPid() +
-      " " +
+      " Clock:" +
       this.getLogical_clock()
     );
   }
